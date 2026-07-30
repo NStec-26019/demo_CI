44b16c6d0d58ce64e1a0eef8c4abf56b122886b7
NStec-26019 <nstec26019fullness>
2026-07-30 16:09:00 +0900

    コードフォーマットの検証を追加

---
+++ src/main/java/com/example/java_actions_app/controller/CalcController.java
@@ -0,0 +1,15 @@
+package com.example.java_actions_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalcController {
+
+    // わざとインデントを崩す例
+    @GetMapping("/calc")
+public String showForm() {
+            return "calc"; 
+    }
+
+}
